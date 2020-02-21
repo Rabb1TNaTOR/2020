7d7e67456b21f35f6690a0600cd2c030db289aba
Denis <d.levchuk@tradesoft.ru>
2020-02-21 07:54:01 +0300

    Проверка на высокосный год

--- test.php
+++ test.php
@@ -1,5 +1,15 @@
 <?php
-echo 'hello world';
-echo "string";
-echo "string";
+function isleap( $year )
+{
+    if( $year % 4 == 0 )
+        # Год високосный
+        echo 'Год високосный';
+    else
+        # Год не високосный
+        echo 'Год не високосный';
+}
+echo isleap(2019);
+
 ?>
+
+
