2e8eb0bbd3526a60069933d80864d4d3097c0fcb
Denis <d.levchuk@tradesoft.ru>
2020-02-21 08:43:08 +0300

    Подсчет суммы цифр числа с использованием рандомного диапазона

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
+//Задача на определение высокосного года
 function isleap( $year )
 {
     if( $year % 4 == 0 )
@@ -9,7 +10,20 @@ function isleap( $year )
         echo 'Год не високосный';
 }
 echo isleap(2019);
-
+<br/>
 ?>
 
-
+<?php
+//Подсчет суммы цифр числа с использованием рандомного диапазона
+$number=rand(11,123);
+$temp=0;
+$summ=0;
+$num=$number;
+while($number!=0)
+  {
+    $temp=$number%10;
+    $summ+=$temp;
+    $number=($number-$temp)/10;
+  }
+print "Случайное число = <b>$num</b><br>Сумма его цифр = <b>$summ</b>";
+?>
